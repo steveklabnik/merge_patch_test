7733c6e8aaaa9e8e3ae10ce90705dd5d45e06d7c
Steve Klabnik <steve@steveklabnik.com>
2013-05-18 14:50:04 -0700

    Initial Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MergePatchTest::Application.config.secret_key_base = '280e3be2afd36c71401b3f98a7ad1bda620e9803857fd7ee5f6cc6229b047360a9ed2486e6ab87136e3a227dd9c56cc1cb4a74397b7fbba4e004c5438c3106aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MergePatchTest::Application.config.session_store :cookie_store, key: '_merge_patch_test_session'
